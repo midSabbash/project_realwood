2ec55a50b5aa1d1155b0662b56b32ed4f6e178fa
Sabbash <Sabbash23@Gmail.com>
2017-01-06 02:45:26 +0200

    last commit

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,40 @@
     });
 })(jQuery);
 
+//calculator
+(function ($) {
+    $(document).ready(function () {
+        $('.calc select').change(function () {
+            var material = Number($('select#material').val());
+            var length = Number($('select#length').val());
+            var sort = Number($('select#sort').val());
+            var width = Number($('select#width').val());
+            var length_room = Number($('select#length-room').val());
+            var width_room = Number($('select#width-room').val());
 
+            var room_area = length_room * width_room;
+            var volume_wood = width * length;
+            var final_price = ((material * sort) * volume_wood) * room_area;
+
+            $('span#price').text(final_price + ' ')
+        })
+    });
+})(jQuery);
+
+//side bar mobile btn
+(function ($){
+    $(window).on('load', function(){
+        var sidebarBtn = $('.mobile-btn-sidebar');
+        var sidebar = $('.sidebar');
+        sidebarBtn.on('click', function (){
+            sidebarBtn.toggleClass('open');
+            sidebar.toggleClass('open');
+        });
+    });
+
+})(jQuery);
+
+//slider
 $('[data-aim="slide"]').on('click', function () {
 
     var attrId = $(this).attr('id');
